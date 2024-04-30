b52d2f8d81b819f45a467bd9bed5d894fc5158bd
Philip <panora@quinnipiac.edu>
2024-04-30 12:04:37 -0400

    framework all done, just needs logic/methods

---
+++ GitOperations.java
@@ -0,0 +1,34 @@
+public class GitOperations {
+    public static void initializeGitRepo(String projectPath) {
+        // Implement Git initialization logic
+    }
+ 
+ 
+    public static void createGitIgnoreFile(String projectPath) {
+        // Implement .gitignore creation logic
+    }
+ 
+ 
+    public static void createReadMeFile(String projectPath, String projectName) {
+        // Implement README.md creation logic
+    }
+ 
+ 
+    public static void makeInitialCommit(String projectPath) {
+        // Implement initial commit logic
+    }
+ 
+ 
+    public static String createGitHubRepo(String repoName, String description, boolean isPrivate) {
+        // Implement GitHub repo creation logic
+        // Return the URL of the newly created GitHub repo
+        return "https://github.com/username/" + repoName;
+    }
+ 
+ 
+    public static void pushToGitHub(String projectPath, String repoUrl) {
+        // Implement logic to push the local repo to GitHub
+    }
+ }
+ 
+ 
